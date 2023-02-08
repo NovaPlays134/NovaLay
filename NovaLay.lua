3cc9d66d9f64c80dcf81eec2139dd8500e4047eb
NovaPlays <luka.verhulst2@gmail.com>
2023-02-08 18:13:08 +0100

    Update NovaLay.lua

--- NovaLay.lua
+++ NovaLay.lua
@@ -43,28 +43,60 @@ local auto_update_config = {
     silent_updates=true,
     dependencies={
         {
+            name="blip",
+            source_url="https://raw.githubusercontent.com/NovaPlays134/NovaLay/main/resources/NovaLay/Blip.png",
+            script_relpath="resources/NovaLay/Blip.png",
+            check_interval=default_check_interval,
+        },
+
+        {
+            name="map",
+            source_url="https://raw.githubusercontent.com/NovaPlays134/NovaLay/main/resources/NovaLay/GTA_Map.png",
+            script_relpath="resources/NovaLay/GTA_Map.png",
+            check_interval=default_check_interval,
+        },
+
+        {
             name="background",
             source_url="https://raw.githubusercontent.com/NovaPlays134/NovaLay/main/resources/NovaLay/mainbackground.png",
             script_relpath="resources/NovaLay/mainbackground.png",
             check_interval=default_check_interval,
         },
 
+        {
+            name="mapbackground",
+            source_url="https://raw.githubusercontent.com/NovaPlays134/NovaLay/main/resources/NovaLay/mapbackground.png",
+            script_relpath="resources/NovaLay/mapbackground.png",
+            check_interval=default_check_interval,
+        },
+
 		{
             name="topborder",
             source_url="https://raw.githubusercontent.com/NovaPlays134/NovaLay/main/resources/NovaLay/topborder.png",
-            script_relpath="resources/NovaMeter/topborder.png",
+            script_relpath="resources/NovaLay/topborder.png",
             check_interval=default_check_interval,
-        }
+        },
+
+        {
+            name="mapborder",
+            source_url="https://raw.githubusercontent.com/NovaPlays134/NovaLay/main/resources/NovaLay/topbordermap.png",
+            script_relpath="resources/NovaLay/topbordermap.png",
+            check_interval=default_check_interval,
+        },
+
     }
 }
 auto_updater.run_auto_update(auto_update_config)
-
 -----------------
 --GETTING FILES--
 -----------------
 resources_dir = filesystem.resources_dir() .. '\\NovaLay\\'
 local topborder = directx.create_texture(resources_dir .. "topborder.png")
 local mainback = directx.create_texture(resources_dir .. "mainbackground.png")
+local mapborder = directx.create_texture(resources_dir .. "topbordermap.png")
+local mapback = directx.create_texture(resources_dir .. "mapbackground.png")
+local map = directx.create_texture(resources_dir .. "GTA_Map.png")
+local blip = directx.create_texture(resources_dir .. "Blip.png")
 
 ----------
 --TABLES--
@@ -78,7 +110,9 @@ local colors = {
         highlight = {["r"] = 160/255, ["g"] = 160/255, ["b"] = 160/255, ["a"] = 1.0}, -- also grayish
         yes_color = {["r"] = 0, ["g"] = 1, ["b"] = 0, ["a"] = 1.0}, --green
         no_color = {["r"] = 1, ["g"] = 0, ["b"] = 0, ["a"] = 1.0}, --red
-        orange = {["r"] = 1, ["g"] = 138/255, ["b"] = 31/255, ["a"] = 1.0}
+        orange = {["r"] = 1, ["g"] = 138/255, ["b"] = 31/255, ["a"] = 1.0},
+        map = {r = 1, g = 1, b = 1, a = 0.75}, --white
+        blip = {["r"] = 1, ["g"] = 1, ["b"] = 1, ["a"] = 1.0} --white
 }
 
 --languages--
@@ -92,14 +126,30 @@ local languages = {
     [6] = "Polish",
     [7] = "Russian",
     [8] = "Korean",
-    [9] = "ChineseTrad",
+    [9] = "Chinese",
     [10] = "Japanese",
     [11] = "Mexican",
-    [12] = "ChineseSimp"
+    [12] = "Chinese"
     }
 -----------
 --OPTIONS--
 -----------
+
+--show map--
+local map_show = true
+local show_map = menu.toggle(menu.my_root(), "Show map on screen", {}, "", function(on)
+    map_show = on
+end, true)
+
+show_map_show = false
+menu.on_focus(show_map, function()
+    show_map_show = true
+end)
+
+menu.on_blur(show_map, function()
+    show_map_show = false
+end)
+
 --lists--
 local color_list = menu.list(menu.my_root(), "Change color's")
 
@@ -194,6 +244,33 @@ menu.on_blur(color_change_high, function()
     color_show_high = false
 end)
 
+--map--
+local color_change_map = menu.colour(color_list, "Change map color", {}, "", colors.map, true, function(color)
+    colors.map = color
+end)
+
+color_show_map = false
+menu.on_focus(color_change_map, function()
+    color_show_map = true
+end)
+
+menu.on_blur(color_change_map, function()
+    color_show_map = false
+end)
+
+--blip--
+local color_change_blip = menu.colour(color_list, "Change Blip color", {}, "", colors.blip, true, function(color)
+    colors.blip = color
+end)
+
+color_show_blip = false
+menu.on_focus(color_change_blip, function()
+    color_show_blip = true
+end)
+
+menu.on_blur(color_change_blip, function()
+    color_show_blip = false
+end)
 --CHANGE VALUE COLORS ON/OFF--
 --bool--
 local bool_change = false
@@ -225,31 +302,39 @@ menu.on_blur(money_change_color, function()
     change_money_show = false
 end)
 
---position-- this is messy asf but ig its the only way if by how i coded the script
+--position-- this is messy asf but ig its the only way by how i coded the script
 local defposX, defposY = 0.215, 0.125
 local backgroundposX, backgroundposY = defposX, defposY
 local topbarposX, topbarposY = defposX, defposY+0.0005
+local mapborderposX, mapborderposY = defposX+0.1911, defposY+0.0005
+local mapbackgroundposX, mapbackgroundposY = defposX+0.1911, defposY
 local nameposX, nameposY = defposX-0.112, defposY-0.035
-local PLAYERposX, PLAYERposY = defposX-0.182, defposY-0.020
-local PLAYERS_textposX, PLAYERS_textposY = defposX-0.195, defposY+0.010
-local CHARACTERposX, CHARACTERposY = defposX-0.050, defposY-0.020
-local CHARACTER_textposX, CHARACTER_textposY = defposX-0.050, defposY+0.010
-local GENERALposX, GENERALposY = defposX-0.117, defposY+0.210
-local GENERAL_textposX, GENERAL_textposY = defposX-0.195, defposY+0.235
+local PLAYERposX, PLAYERposY = defposX-0.050, defposY-0.020
+local PLAYERS_textposX, PLAYERS_textposY = defposX-0.050, defposY+0.010
+local CHARACTERposX, CHARACTERposY = defposX-0.182, defposY-0.020
+local CHARACTER_textposX, CHARACTER_textposY = defposX-0.195, defposY+0.010
+local GENERALposX, GENERALposY = defposX-0.117, defposY+0.200
+local GENERAL_textposX, GENERAL_textposY = defposX-0.195, defposY+0.220
 local tagsposX, tagsposY = defposX-0.195, defposY+0.375
+local mapposX, mapposY = defposX+0.096159587, defposY+0.18583594
+local blipposX, blipposY = defposX+0.0079167, defposY-0.00740741
 
 --x--
 local posXslider = menu.slider(menu.my_root(), "Speedometer pos X", {}, "Default value is 215", 1, 1000, defposX*1000, 1, function(x)
     backgroundposX = x/1000
     topbarposX = x/1000
+    mapborderposX = x/1000 + 0.1911
+    mapbackgroundposX = x/1000 + 0.1911
     nameposX = x/1000 - 0.112
-    PLAYERposX = x/1000 - 0.182
-    PLAYERS_textposX = x/1000 - 0.195
-    CHARACTERposX = x/1000 - 0.050
-    CHARACTER_textposX = x/1000 - 0.050
+    PLAYERposX = x/1000 - 0.050
+    PLAYERS_textposX = x/1000 - 0.050
+    CHARACTERposX = x/1000 - 0.182
+    CHARACTER_textposX = x/1000 - 0.195
     GENERALposX = x/1000 - 0.117
     GENERAL_textposX = x/1000 - 0.195
     tagsposX = x/1000 - 0.195
+    mapposX = x/1000 + 0.096159587
+    blipposX = x/1000 + 0.0079167
 end)
 
 posXslider_show = false
@@ -265,14 +350,18 @@ end)
 local posYslider = menu.slider(menu.my_root(), "Speedometer pos Y", {}, "Default value is 125", 1, 1000, defposY*1000, 1, function(y)
     backgroundposY = y/1000
     topbarposY = y/1000 + 0.0005
+    mapborderposY = y/1000 +0.0005
+    mapbackgroundposY = y/1000
     nameposY = y/1000 - 0.035
     PLAYERposY = y/1000 - 0.020
     PLAYERS_textposY = y/1000 + 0.010
     CHARACTERposY = y/1000 - 0.020
     CHARACTER_textposY = y/1000 + 0.010
-    GENERALposY = y/1000 + 0.210
-    GENERAL_textposY = y/1000 + 0.235
+    GENERALposY = y/1000 + 0.200
+    GENERAL_textposY = y/1000 + 0.220
     tagsposY = y/1000 + 0.375
+    mapposY = y/1000 + 0.18583594
+    blipposY = y/1000 - 0.00740741
 end)
 
 posYslider_show = false
@@ -282,6 +371,20 @@ end)
 
 menu.on_blur(posYslider, function()
     posYslider_show = false
+end)
+
+local blipsize = 0.0035
+local blipsizeslider = menu.slider(menu.my_root(), "Change blip size", {}, "Default value is 35", 1, 100, blipsize*10000, 1, function(size)
+    blipsize = size/10000
+end)
+
+blipsizeslider_show = false
+menu.on_focus(blipsizeslider, function()
+    blipsizeslider_show = true
+end)
+
+menu.on_blur(blipsizeslider, function()
+    blipsizeslider_show = false
 end)
 
 --change measurement--
@@ -299,17 +402,21 @@ menu.on_blur(change_unit, function()
     change_unit_show = false
 end)
 
-menu.action(menu.my_root(), "Check for Update", {}, "The script will automatically check for updates at most daily, but you can manually check using this option anytime.", function()
+local info_list = menu.list(menu.my_root(), "Info")
+
+menu.action(info_list, "Credits to lev for the map thingy", {}, "", function()
+end)
+
+menu.action(info_list, "Check for Update", {}, "The script will automatically check for updates at most daily, but you can manually check using this option anytime.", function()
     auto_update_config.check_interval = 0
     if auto_updater.run_auto_update(auto_update_config) then
         util.toast("No updates found")
     end
 end)
-menu.action(menu.my_root(), "Clean Reinstall", {}, "Force an update to the latest version, regardless of current version.", function()
+menu.action(info_list, "Clean Reinstall", {}, "Force an update to the latest version, regardless of current version.", function()
     auto_update_config.clean_reinstall = true
     auto_updater.run_auto_update(auto_update_config)
 end)
-
 -------------
 --FUNCTIONS--
 -------------
@@ -357,8 +464,8 @@ function check(info)
 end
 
 function queuecheck(info)
-    if info == nil or info == "NULL" or info == 0 or info == " " then
-        return "Not in queue"
+    if info == nil or info == "NULL" or info == " " then
+        return 0
     else
         return "#" .. info
     end
@@ -412,6 +519,10 @@ function draw_info_text(text, infotext, posX, posY, distance, hight, size1, size
     end
 end
 
+function get_prostitutes_solicited(pid)
+    return memory.read_int(memory.script_global(1853348 + 1 + (pid * 834) + 205 + 54))
+end
+
 function green_or_red(yes_no)
     if bool_change then
         return colors.highlight
@@ -444,9 +555,9 @@ end
 util.create_tick_handler(function()
     if not util.is_session_transition_active() then
         local focusedplayer = players.get_focused()
-        if focusedplayer[1] ~= nil and menu.is_open() or (color_show_list or value_show_list or color_show_topbar or color_show_back or color_show_sub or color_show_label or color_show_high or change_bool_show or change_money_show or change_unit_show or posXslider_show or posYslider_show) then
+        if focusedplayer[1] ~= nil and menu.is_open() or (color_show_list or value_show_list or color_show_topbar or color_show_back or color_show_sub or color_show_label or color_show_high or color_show_map or color_show_blip or change_bool_show or change_money_show or change_unit_show or posXslider_show or posYslider_show or blipsizeslider_show or show_map_show) then
 
-            if (color_show_list or value_show_list or color_show_topbar or color_show_back or color_show_sub or color_show_label or color_show_high or change_bool_show or change_money_show or change_unit_show or posXslider_show or posYslider_show) then
+            if (color_show_list or value_show_list or color_show_topbar or color_show_back or color_show_sub or color_show_label or color_show_high or color_show_map or color_show_blip or change_bool_show or change_money_show or change_unit_show or posXslider_show or posYslider_show or blipsizeslider_show or show_map_show) then
                 focusedplayer = players.user()
             else
                 focusedplayer = focusedplayer[1]
@@ -494,14 +605,37 @@ util.create_tick_handler(function()
                 local weapon =  get_weapon_name_from_hash(weapon_hash)
                 local host = bool(focusedplayer == players.get_host())
                 local script_host = bool(focusedplayer == players.get_script_host())
-
+                local hookers = get_prostitutes_solicited(focusedplayer)
             --topbar--
 		    directx.draw_texture(topborder, 0.12, 0.12, 0.88, 0.125, topbarposX, topbarposY, 0.0, colors.topbar)
             --background--
             directx.draw_texture(mainback, 0.12, 0.12, 0.88, 0.125, backgroundposX, backgroundposY, 0.0, colors.background)
 
+        -------
+        --MAP--
+        -------
+        if map_show then
+            --mapborder--
+            directx.draw_texture(mapborder, 0.12, 0.12, 0.88, 0.125, mapborderposX, mapborderposY, 0.0, colors.topbar)
+            --mapbackground--
+            directx.draw_texture(mapback, 0.12, 0.12, 0.88, 0.125, mapbackgroundposX, mapbackgroundposY, 0.0, colors.background)
+
+            --im not happy about this code tbh to many fucked up numbers for me and i just copied this shit from InfOverlay from lev maybe if u read this i havnt changed it since its been added but if u dont then its better :D
+            --map--
+            local mapsizeX, mapsizeY = 0.14416506, 0.3864867
+            directx.draw_texture(map, mapsizeX/2, mapsizeY, 0.5, 0.5, mapposX, mapposY, 0.0, colors.map)
+
+            --blip--
+            local blip_posX = blipposX + 0.0161603566 + ((playerpos.x + 3745)/8316) * mapsizeX
+            local blip_posY = blipposY + (1 - (playerpos.y + 4427)/12689) * mapsizeY
+            local heading = ENTITY.GET_ENTITY_HEADING(focusedped)
+            if blip_posX <= 0.39064228813 and blip_posX >= 0.2383465800 and blip_posY >= 0.103608880 and blip_posY <= 0.530602210 then
+                directx.draw_texture(blip, blipsize, blipsize, 0.5, 0.5, blip_posX, blip_posY, (360 - heading)/360, colors.blip)
+            end
+        end
             --name--
             directx.draw_text(nameposX, nameposY, name, ALIGN_TOP_LEFT, 0.45, colors.subhead, true)
+
 
             --PLAYER--
             directx.draw_text(PLAYERposX, PLAYERposY, "Player Info", ALIGN_TOP_LEFT, 0.5, colors.subhead, true)
@@ -559,6 +693,8 @@ util.create_tick_handler(function()
             draw_info_text("Wallet:", "$" .. walletmoney, GENERAL_textposX, GENERAL_textposY + 0.08, 192, 100, 0.45, 0.44, money_color(players.get_wallet(focusedplayer)))
             --bank money--
             draw_info_text("Bank:", "$" .. bankmoney, GENERAL_textposX, GENERAL_textposY + 0.1, 192, 100, 0.45, 0.44, money_color(players.get_bank(focusedplayer)))
+            --hookers--
+            draw_info_text("Hookers:", hookers, GENERAL_textposX, GENERAL_textposY + 0.12, 192, 100, 0.45, 0.44)
             --tags--
             draw_info_text("Tags:", tags, tagsposX, tagsposY, 22, 20, 0.45, 0.44)
         end
